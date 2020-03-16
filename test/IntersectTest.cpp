4826a0bf3b25d2fe26a5634be64ad4c9624349eb
Jiyuan-Yang <jim_9827@icloud.com>
2020-03-16 16:59:05 +0800

    add Intersect test

---
+++ test/IntersectTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../src/Shape.h"
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test
+{
+	TEST_CLASS(test)
+	{
+	public:
+		TEST_METHOD(MethodTest)
+		{
+			
+		}
+	};
+}
