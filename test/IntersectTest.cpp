f3c775440ccc2f3b5e2ea28b98f0d3cc33a65a59
Jiyuan-Yang <jim_9827@icloud.com>
2020-03-19 15:50:52 +0800

    update test

--- test/IntersectTest.cpp
+++ test/IntersectTest.cpp
@@ -3,6 +3,7 @@
 #include "../src/Shape.h"
 #include "../src/Intersection.h"
 #include <cmath>
+#include <fstream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -13,99 +14,81 @@ namespace test
 	public:
 		TEST_METHOD(CC0Test)
 		{
-			if (freopen("../testcase/cc_0.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/cc_0.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 23);
 		}
 
 		TEST_METHOD(RL0Test)
 		{
-			if (freopen("../testcase/rl_0.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/rl_0.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 13);
 		}
 
 		TEST_METHOD(SL0Test)
 		{
-			if (freopen("../testcase/sl_0.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/sl_0.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 11);
 		}
 
 		TEST_METHOD(RSC0Test)
 		{
-			if (freopen("../testcase/rsc_0.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/rsc_0.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 11);
 		}
 
 		TEST_METHOD(RSLC0Test)
 		{
-			if (freopen("../testcase/rslc_0.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/rslc_0.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 17);
 		}
 
 		TEST_METHOD(RSL1Test)
 		{
-			if (freopen("../testcase/rsl_1.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/rsl_1.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 14);
 		}
 
 		TEST_METHOD(RSLC1Test)
 		{
-			if (freopen("../testcase/rslc_1.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/rslc_1.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 12);
 		}
 
 		TEST_METHOD(LC1Test)
 		{
-			if (freopen("../testcase/lc_1.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/lc_1.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 9);
 		}
 
 		TEST_METHOD(RSL0Test)
 		{
-			if (freopen("../testcase/rsl_0.txt", "r", stdin) == NULL) {
-				Assert::AreEqual(1, 2);
-			}
+			ifstream in("../testcase/rsl_0.txt");
 			Intersection* intersect = new Intersection();
-			intersect->getAllPoints();
+			intersect->getAllPoints(in);
 			int ret = intersect->solveIntersection();
 			Assert::AreEqual(ret, 15);
 		}
